9efd06a222fccef4fb7021a86a13805e4614291f
Xaphan67 <cedric.falda@gmail.com>
2023-09-15 11:36:06 +0200

    Ajout : Informations détaillées Entreprises et Employés

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -13,9 +13,17 @@ class EntrepriseController extends AbstractController
     #[Route('/entreprise', name: 'app_entreprise')]
     public function index(EntrepriseRepository $entrepriseRepository): Response
     {
-        $entreprises = $entrepriseRepository->findBy(["ville" => "STRASBOURG"], ["raisonSociale" => "ASC"]);
+        $entreprises = $entrepriseRepository->findBy([], ["raisonSociale" => "ASC"]);
         return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises
+        ]);
+    }
+
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise) : Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
         ]);
     }
 }
